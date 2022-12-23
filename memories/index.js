489f0ea3d707330db97ebd5e2e646a8a124ccf50
Yu-Ping Wu <yupingso@chromium.org>
2022-12-23 19:36:39 +0800

    Move "loading" animation to html

--- memories/index.js
+++ memories/index.js
@@ -56,14 +56,11 @@ function closecontact() {
         $("#contact_container").css("display", "none");
     }, 800);
 }
+
 setTimeout(function () {
-    $("#loading").addClass("animated fadeOut");
-    setTimeout(function () {
-        $("#loading").removeClass("animated fadeOut");
-        $("#loading").css("display", "none");
-        $("#box").css("display", "none");
-    }, 1000);
-}, 1500);
+    $("#loading").css("display", "none");
+    $("#box").css("display", "none");
+}, 2500);
 
 // Countdown timer
 
